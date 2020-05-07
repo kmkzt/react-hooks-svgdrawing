1316073929fcfd3bfa9ecb086df6b4f7eae44aea
kmkzt <info.pscreator@gmail.com>
2020-05-07 22:21:49 +0900

    build(lib directory): remove lib directory on git

--- lib/utils/downloadBlob.d.ts
+++
@@ -1,7 +0,0 @@
-interface DownloadParam {
-    base64: string;
-    filename: string;
-    mimeType: string;
-}
-export declare const downloadBlob: ({ base64, filename, mimeType }: DownloadParam) => void;
-export {};
